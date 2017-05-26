13b26caa75fb4e9a494f002069c3feda507255c2
sdei-sarvesh <sarveshd@smartdatainc.net>
2017-05-26 15:19:00 +0530

    Add files via upload

--- config/auth.js
+++ config/auth.js
@@ -1,1 +1,11 @@
+// config/auth.js
 
+// expose our config directly to our application using module.exports
+module.exports = {
+	'facebookAuth': {
+		'clientID': '161428167658142', // your App ID
+		'clientSecret': '63801b3933784acc73299823eb97300a', // your App Secret
+		'scope': 'public_profile, publish_actions, publish_pages, email, manage_pages, user_photos, business_management, user_friends',
+		'callbackURL': 'http://fbtesttask.tk:5058/auth/facebook/callback'
+	},
+};
---
+++ config/database.js
@@ -0,0 +1,4 @@
+// config/database.js
+module.exports = {
+	  'url' : 'mongodb://fbtesttasks:fbtesttasks@ds151941.mlab.com:51941/fbtesttasks'  // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};
